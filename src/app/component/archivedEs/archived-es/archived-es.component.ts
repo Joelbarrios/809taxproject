35d72effc58a8a941f39c7af62a6a49c836e1e0e
Joel Barrios <joelbh.ccs@gmail.com>
2024-01-07 00:03:02 -0300

    mejorando

--- src/app/component/archivedEs/archived-es/archived-es.component.ts
+++ src/app/component/archivedEs/archived-es/archived-es.component.ts
@@ -69,8 +69,8 @@ export class ArchivedEsComponent {
    // Método para filtrar la lista
    filtrarFormData() {
     return this.formData.filter(data =>
-      data.name.toLowerCase().includes(this.terminoBusqueda.toLowerCase()) ||
-      data.spouse_name.toLowerCase().includes(this.terminoBusqueda.toLowerCase()) ||
+      data.nombre.toLowerCase().includes(this.terminoBusqueda.toLowerCase()) ||
+      data.nombre_del_esposo.toLowerCase().includes(this.terminoBusqueda.toLowerCase()) ||
       data.mail.toLowerCase().includes(this.terminoBusqueda.toLowerCase())
     );
   }
